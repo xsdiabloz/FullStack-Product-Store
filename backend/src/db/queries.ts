31022919948835d06cd54b11c8f9834e1cc2d2cf
xsdiablozb <cactusjackk48@gmail.com>
2026-03-26 13:13:28 +0200

    add user, product, comment query functions to handle db operations

---
+++ backend/src/db/queries.ts
@@ -0,0 +1,93 @@
+import { eq } from "drizzle-orm";
+import type { NewComment, NewProduct, NewUser } from "../types/types.js";
+import { users, products, comments } from "./schema.js";
+import { db } from "./index.js";
+
+export const createUser = async (data: NewUser) => {
+  const [user] = await db.insert(users).values(data).returning();
+  return user;
+};
+
+export const getUserById = async (id: string) => {
+  return db.query.users.findFirst({ where: eq(users.id, id) });
+};
+
+export const updateUser = async (id: string, data: Partial<NewUser>) => {
+  const [user] = await db
+    .update(users)
+    .set(data)
+    .where(eq(users.id, id))
+    .returning();
+  return user;
+};
+
+export const upsertUser = async (data: NewUser) => {
+  const existingUser = await getUserById(data.id);
+  if (existingUser) return updateUser(data.id, data);
+  return createUser(data);
+};
+
+export const createProduct = async (data: NewProduct) => {
+  const [product] = await db.insert(products).values(data).returning();
+  return product;
+};
+
+export const getAllProducts = async () => {
+  return db.query.products.findMany({
+    with: { user: true },
+    orderBy: (products, { desc }) => [desc(products.createdAt)],
+  });
+};
+
+export const getProductById = async (id: string) => {
+  return db.query.products.findFirst({
+    where: eq(products.id, id),
+    with: {
+      user: true,
+      comments: {
+        with: { user: true },
+        orderBy: (comments, { desc }) => [desc(comments.createdAt)],
+      },
+    },
+  });
+};
+
+export const getProductsByUserId = async (userId: string) => {
+  return db.query.products.findMany({
+    where: eq(products.userId, userId),
+    with: {
+      user: true,
+    },
+    orderBy: (products, { desc }) => [desc(products.createdAt)],
+  });
+};
+
+export const updateProduct = async (id: string, data: Partial<NewProduct>) => {
+  const [product] = await db
+    .update(products)
+    .set(data)
+    .where(eq(products.id, id))
+    .returning();
+  return product;
+};
+
+export const deleteProduct = async (id: string) => {
+  const [product] = await db
+    .delete(products)
+    .where(eq(products.id, id))
+    .returning();
+  return product;
+};
+
+export const createComment = async (data: NewComment) => {
+  const [comment] = await db.insert(comments).values(data).returning();
+  return comment;
+};
+
+export const deleteComment = async (id: string) => {
+  const [comment] = await db
+    .delete(comments)
+    .where(eq(comments.id, id))
+    .returning();
+  return comment;
+};
